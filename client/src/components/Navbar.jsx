9b4d7c0ccd7bf719563c5485d24fbdfc41c834c8
baljitsingh-alt <baljit@altrodav.com>
2025-08-23 16:29:48 +0530

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -140,7 +140,7 @@ export default function Navbar() {
                 </div>
               </div>
             ) : (
-              <div className="flex items-center space-x-4">
+              <div className="hidden md:block items-center space-x-4">
                 <Link to="/login" className="text-gray-500 hover:text-gray-700 text-sm font-medium">
                   Login
                 </Link>
